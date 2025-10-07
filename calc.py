63bf4d0f2d6cceaf9f039d3e065f1c5975bb399e
Abrar Nadeem <abrarnadeem007@gmail.com>
2025-10-07 15:37:43 +0100

    Fix bug in triangular numbers function

--- calc.py
+++ calc.py
@@ -4,7 +4,7 @@ def triangular_numbers(n):
     if n < 1 or n != int(n):
         raise ValueError("Must be a positive integer")
     else:
-        return n * (n+2) // 2
+        return n * (n+1) // 2
     
 print(triangular_numbers(100))
 
