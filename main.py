6b906118362ee42ceed943a383ae7fc347a393db
Abrar Nadeem <abrarnadeem007@gmail.com>
2025-11-23 21:39:31 +0000

    Remove unnecessary code

--- main.py
+++ main.py
@@ -1,23 +1,3 @@
-# import streamlit as st
-# import st_pages
-# from st_pages import _get_pages_from_config
-
-# # # Load pages from the toml config file automatically
-# st_pages.StreamlitPage("Week_2/compound_interest_calculator.py", title="hey")
-# st_pages.StreamlitPage("Week_3/calendar_printer.py", title="hey")
-# # st.write("HEY!")
-# #st.write(dir(st_pages))
-
-# #from st_pages import add_page, show_pages
-
-# # add_page("Week_2/compound_interest_calculator.py", "Compound Interest Calculator")
-# # add_page("Week_3/calendar_printer.py", "Calendar Printer")
-# # # add other pages
-
-
-# # import streamlit as st
-# # st.write("HEY!")
-
 import streamlit as st
 
 pages = {
