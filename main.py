6241488c9253d64ff6aea3b0c436295fcec013cd
Abrar Nadeem <abrarnadeem007@gmail.com>
2025-11-27 12:34:28 +0000

    Add relevant information

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Hey Ed, in order to run all project files, write streamlit run main.py in a new terminal
+
 import streamlit as st
 
 pages = {
@@ -10,12 +12,16 @@ pages = {
     "Week 4": [
         st.Page("Week_4/Datetime_calculator.py", title="Datetime calculator"),
     ],
-    "Week 5": [
+    "Weeks 5,6 and 7": [
         st.Page("Week_5/asset_prices.py", title="Asset Prices"),
         st.Page("Week_5/election_data.py", title="2016 Election Data")
     ],
-    "Week 8": [
+    "Weeks 8 and 9": [
         st.Page("Week_8/fertility_rates_usa_fitting_forecasting.py", title="USA Fertility Rates Forecasting"),
+        st.Page("Week_8/Week_8_README.py", title="Week 8 README")
+    ],
+        "Week 10": [
+        st.Page("Week_4/example.py", title="Example"),
     ],
 }
 
