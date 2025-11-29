a3dce6c09828b7d5d55b3384d68795660d827762
Abrar Nadeem <abrarnadeem007@gmail.com>
2025-11-29 23:09:30 +0000

    Made some changes

--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 import streamlit as st
 
 pages = {
+    "Before you check other pages...": [
+        st.Page("hey.py", title="READ THIS!"),
+    ],
     "Week 2": [
         st.Page("Week_2/compound_interest_calculator.py", title="Compound Interest Calculator"),
     ],
@@ -17,11 +20,16 @@ pages = {
         st.Page("Week_5/election_data.py", title="2016 Election Data")
     ],
     "Weeks 8 and 9": [
-        st.Page("Week_8/fertility_rates_usa_fitting_forecasting.py", title="USA Fertility Rates Forecasting"),
-        st.Page("Week_8/Week_8_README.py", title="Week 8 README")
+        st.Page("Week_8/1_fertility_rates_usa_fitting_forecasting.py", title="USA Fertility Rates Forecasting"),
+        st.Page("Week_8/2_fertility_rates_usa_dof_bic.py", title="DOF and BIC graphs"),
+        st.Page("Week_8/3_fertility_rates_usa_best_order.py", title="Best Order Model"),
+        st.Page("Week_8/4_gaussian_model_fitting.py", title="Gaussian Fit"),
+        st.Page("Week_8/5_emcee_fit.py", title="Using emcee module"),
+        st.Page("Week_8/6_scikit_learn_model.py", title="Forecasting with sci-kit learn")
     ],
         "Week 10": [
-        st.Page("Week_4/example.py", title="Example"),
+        st.Page("Week_10/decision_tree.py", title="Decision Tree"),
+        st.Page("Week_10/varying_depth.py", title="Varying Depth and Feature importance")
     ],
 }
 
